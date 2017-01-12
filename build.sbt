054db497d8c32891f7ece84888405bd501c0a8b8
Chen Zhao <chen.zhao@xad.com>
2017-01-11 18:35:47 -0800

    update

--- build.sbt
+++ build.sbt
@@ -3,4 +3,8 @@ name := "akka_stream_playground"
 version := "1.0"
 
 scalaVersion := "2.12.1"
+
+libraryDependencies += (
+  "com.typesafe.akka" %% "akka-stream" % "2.4.16"
+)
     
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,51 @@
+import java.nio.file.Paths
+
+import akka.{Done, NotUsed}
+import akka.actor.ActorSystem
+import akka.stream.{ThrottleMode, IOResult, ActorMaterializer}
+import akka.stream.scaladsl._
+import akka.util.ByteString
+import scala.concurrent.duration._
+
+import scala.concurrent.Future
+
 object Main extends App {
 
+  implicit val system = ActorSystem("system")
+  implicit val materializer = ActorMaterializer()
+
+//  example1
+//  example2
+  example4
+
+  def example1 = {
+    val source: Source[Int, NotUsed] = Source(1 to 100)
+    source.runForeach { i => println(i) }(materializer)
+  }
+
+  def example2 = {
+    val source: Source[Int, NotUsed] = Source(1 to 100)
+    val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
+    val result: Future[IOResult] =
+      factorials.
+        map(num => ByteString(s"$num\n")).
+        runWith(FileIO.toPath(Paths.get("factorials.txt")))
+  }
+
+  // reusable pieces
+  def example3LineSink(filename: String): Sink[String, Future[IOResult]] =
+    Flow[String]
+      .map(s => ByteString(s + "\n"))
+      .toMat(FileIO.toPath(Paths.get(filename)))(Keep.right)
+
+
+  def example4 = {
+    val source: Source[Int, NotUsed] = Source(1 to 100)
+    val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
+    val done: Future[Done] =
+      factorials
+        .zipWith(Source(1 to 100))((num, idx) => s"$idx! = $num")
+        .throttle(1, 1.second, 1, ThrottleMode.shaping)
+        .runForeach(println)
+  }
 }
